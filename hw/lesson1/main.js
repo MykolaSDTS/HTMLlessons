f03d45f67d388d215c1073d3d1a1ed18296e2c20
MykolaSTDS <mykola94pc@gmail.com>
2022-04-29 00:25:20 +0300

    1

--- hw/lesson1/main.js
+++ hw/lesson1/main.js
@@ -1,47 +1,58 @@
 // - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
 //     Вивести кожну змінну за допомогою: console.log , alert, document.write
 //
-    let a1 = 'hello';
-    let a2 = 'owu';
-    let a3 = 'ua';
-    let a4 = 1;
-    let a5 = 10;
-    let a6 = -999;
-    let a7 = 123;
-    let a8 = 3.14;
-    let a9 = 2.7;
-    let a10 = 16;
-    let a11 = true;
-    let a12 = false;
-    let a13 = 'com';
-    console.log(a1);
-    console.log(a2);
-    console.log(a3);
-    console.log(a4);
-    console.log(a5);
-    console.log(a6);
-    console.log(a7);
-    console.log(a8);
-    console.log(a9);
-    console.log(a10);
-    console.log(a11);
-    console.log(a12);
-    console.log(a13);
-    document.write(a1)
-    document.write(a2)
-    document.write(a3)
-    document.write(a4)
-    document.write(a5)
-    document.write(a6)
-    document.write(a7)
-    document.write(a8)
-    document.write(a9)
-    document.write(a10)
-    document.write(a11)
-    document.write(a12)
-    document.write(a13)
-
-
+//     let a1 = 'hello';
+//     let a2 = 'owu';
+//     let a3 = 'ua';
+//     let a4 = 1;
+//     let a5 = 10;
+//     let a6 = -999;
+//     let a7 = 123;
+//     let a8 = 3.14;
+//     let a9 = 2.7;
+//     let a10 = 16;
+//     let a11 = true;
+//     let a12 = false;
+//     let a13 = 'com';
+//     console.log(a1);
+//     console.log(a2);
+//     console.log(a3);
+//     console.log(a4);
+//     console.log(a5);
+//     console.log(a6);
+//     console.log(a7);
+//     console.log(a8);
+//     console.log(a9);
+//     console.log(a10);
+//     console.log(a11);
+//     console.log(a12);
+//     console.log(a13);
+//     document.write(a1);
+//     document.write(a2);
+//     document.write(a3);
+//     document.write(a4);
+//     document.write(a5);
+//     document.write(a6);
+//     document.write(a7);
+//     document.write(a8);
+//     document.write(a9);
+//     document.write(a10);
+//     document.write(a11);
+//     document.write(a12);
+//     document.write(a13);
+//     alert(a1);
+//     alert(a2);
+//     alert(a3);
+//     alert(a4);
+//     alert(a5);
+//     alert(a6);
+//     alert(a7);
+//     alert(a8);
+//     alert(a9);
+//     alert(a10);
+//     alert(a11);
+//     alert(a12);
+//     alert(a13);
 
 
 // - Створити об'єкт book з наступними полями :
@@ -49,6 +60,12 @@
 // кількість сторінок (числовий тип),
 // жанр (string)
 //
+// let book1 = {
+//     title: 'animals',
+//     p: 47,
+//     genre: 'future',
+// };
+// console.log(book);
 
 
 // - Створити об'єкт book з наступними полями :
@@ -57,7 +74,16 @@
 // жанр (string)
 // автори (тип - масив, кожен елемент масиву - це стрінга)
 //
-
+// let book2 = {
+//     title: 'animals',
+//     p: 100,
+//     genre: 'future',
+//     authors : [
+//         'gdgsgggr',
+//         'ffggrggsr'
+//     ]
+// }
+// console.log(book2);
 
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 //
