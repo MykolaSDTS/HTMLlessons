e7b13e4774d9de2fbbc1b83c64b0454db6841ede
MykolaSTDS <mykola94pc@gmail.com>
2022-04-29 00:07:49 +0300

    Initial commit

---
+++ hw/lesson1/main.js
@@ -0,0 +1,75 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+//
+    let a1 = 'hello';
+    let a2 = 'owu';
+    let a3 = 'ua';
+    let a4 = 1;
+    let a5 = 10;
+    let a6 = -999;
+    let a7 = 123;
+    let a8 = 3.14;
+    let a9 = 2.7;
+    let a10 = 16;
+    let a11 = true;
+    let a12 = false;
+    let a13 = 'com';
+    console.log(a1);
+    console.log(a2);
+    console.log(a3);
+    console.log(a4);
+    console.log(a5);
+    console.log(a6);
+    console.log(a7);
+    console.log(a8);
+    console.log(a9);
+    console.log(a10);
+    console.log(a11);
+    console.log(a12);
+    console.log(a13);
+    document.write(a1)
+    document.write(a2)
+    document.write(a3)
+    document.write(a4)
+    document.write(a5)
+    document.write(a6)
+    document.write(a7)
+    document.write(a8)
+    document.write(a9)
+    document.write(a10)
+    document.write(a11)
+    document.write(a12)
+    document.write(a13)
+
+
+
+
+// - Створити об'єкт book з наступними полями :
+// назва, (тип string)
+// кількість сторінок (числовий тип),
+// жанр (string)
+//
+
+
+// - Створити об'єкт book з наступними полями :
+// назва, (тип string)
+// кількість сторінок (числовий тип),
+// жанр (string)
+// автори (тип - масив, кожен елемент масиву - це стрінга)
+//
+
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+//
+
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+//
+
+
+// - Створити масив об'єктів з назвою books з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
